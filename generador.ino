f184794743b7f353b3d7c30e84c24c2b45b6eebb
Turri <inigoturrientes@gmail.com>
2020-08-13 19:50:55 +0200

    First commit.

---
+++ generador.ino
@@ -0,0 +1,12 @@
+void setup() {
+
+  #define boyaGasolina A0
+  
+  // put your setup code here, to run once:
+  pinMode(boyaGasolina, INPUT);
+} 
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
